f3a264f119db04af2443faaf1103e3c2bc97ded5
Maayan Israel <maayan@b01-aruba-authenticated-10-110-199-106.wlan.cpp.edu>
2025-02-17 22:34:32 -0800

    added CORS to allow requests from our front end local instance, schema change for schema to receive clerk id

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,7 @@ from app.models.database import db
 class User(db.Model):
     __tablename__ = 'users'
     
-    userId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userId = db.Column(db.String(255), primary_key=True, nullable=False)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     spotifyId = db.Column(db.String(255), unique=True, nullable=True)
