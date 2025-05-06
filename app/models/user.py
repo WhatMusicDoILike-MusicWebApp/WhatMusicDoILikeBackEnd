28060e27ce278628dfb2835bbe0d25e8bd01774e
Maayan Israel <maayan@Maayans-MacBook-Pro.local>
2025-05-05 23:19:31 -0700

    fixed users table

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     spotifyId = db.Column(db.String(255), unique=True, nullable=True)
-    youtubeId = db.Column(db.String(255), nullable=True)
+    youtubeId = db.Column(db.JSON, nullable=True)
     appleMusicId = db.Column(db.String(255), unique=True, nullable=True)
     spotifyAuthToken = db.Column(db.String(500), nullable=True)
     spotifyRefreshToken = db.Column(db.String(500), nullable=True)
