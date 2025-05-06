80c5ba68deb4086a10f21d1aa567d83764245ece
Maayan Israel <maayan@Maayans-MacBook-Pro.local>
2025-05-05 22:21:13 -0700

    fixed finally

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -5,8 +5,4 @@ from .youtube_auth import youtube_auth_bp
 from .playlist_routes import playlist_bp
 from .spotify_transfer import spotify_transfer_bp
 from .health_checks import health_checks
-<<<<<<< HEAD
 from .spotify_transfer import spotify_transfer_bp
-=======
-
->>>>>>> 2084688a74a66f450a6e5d744f385d99236b895f
